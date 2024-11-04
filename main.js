dc3b1e164183b74a5de4fa2d905052b3d8d28d1a
maha <bekri.mehe@gmail.come>
2024-11-04 19:33:17 +0300

    first

---
+++ main.js
@@ -0,0 +1,39 @@
+
+// challenge 1
+
+let num1 = 10;
+let num2 = 3;
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+let remainder = num1 % num2;
+
+
+console.log(sum)
+console.log(difference)
+console.log(product)
+console.log(quotient)
+console.log(remainder)
+
+
+// challenge 2
+
+let isGreater = num1 > num2;
+let isLess = num1 < num2;
+let isEqual = num1 === num2;
+let isNotEqual = num1 !== num2;
+console.log(isGreater);
+console.log(isLess);
+console.log(isEqual);
+console.log(isNotEqual);
+
+
+
+
+// challenge 3
+
+let firstName = "Maha"
+let lastName = "BEKRI"
+
+console.log(`${firstName} ${lastName}`)
